88d36e9aa2de491dad859bc0ff5bd116ee3b6858
keshanwen <3640@winnermedical.com>
2024-02-18 14:50:18 +0800

    feat: 回溯算法，子集和

--- src/Algorithm/backtracking/index.ts
+++ src/Algorithm/backtracking/index.ts
@@ -1,28 +1,32 @@
 
 
+/* 回溯算法：子集和 II */
 function backtrack(
-  state: number[],
-  target: number,
-  choices: number[],
-  start: number,
-  res: number[][]
+    state: number[],
+    target: number,
+    choices: number[],
+    start: number,
+    res: number[][]
 ): void {
   if (target === 0) {
     res.push([...state])
     return
   }
-
   for (let i = start; i < choices.length;i++) {
     if (target - choices[i] < 0) {
       break
     }
+    if (i > start && choices[i] === choices[i-1]) {
+      continue
+    }
     state.push(choices[i])
-    backtrack(state, target - choices[i], choices, i, res)
+    backtrack(state, target - choices[i], choices, i + 1, res)
     state.pop()
   }
 }
 
-function subsetSumI(nums: number[], target: number):number[][] {
+/* 求解子集和 II */
+function subsetSumII(nums: number[], target: number): number[][] {
   const state = []
   nums.sort((a, b) => a - b)
   const start = 0
@@ -32,5 +36,4 @@ function subsetSumI(nums: number[], target: number):number[][] {
 }
 
 
-
-//console.log(subsetSumI([3,4,5],9))
+console.log(subsetSumII([3, 4, 5], 9));
