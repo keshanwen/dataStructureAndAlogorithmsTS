c397abc319975f664b80818bdb22813ba73b5b02
keshanwen <3640@winnermedical.com>
2024-02-18 16:46:28 +0800

    feat(回溯算法): N 皇后

--- src/Algorithm/backtracking/index.ts
+++ src/Algorithm/backtracking/index.ts
@@ -1,39 +1,41 @@
-
-
-/* 回溯算法：子集和 II */
 function backtrack(
-    state: number[],
-    target: number,
-    choices: number[],
-    start: number,
-    res: number[][]
+  row: number,
+  n: number,
+  state: string[][],
+  res: string[][][],
+  cols: boolean[],
+  diags1: boolean[],
+  diags2: boolean[]
 ): void {
-  if (target === 0) {
-    res.push([...state])
+  if (row === n) {
+    res.push(state.map((row) => row.slice()))
     return
   }
-  for (let i = start; i < choices.length;i++) {
-    if (target - choices[i] < 0) {
-      break
+
+  for (let col = 0; col < n; col++) {
+    const diag1 = row - col + n - 1
+    const diag2 = row + col
+    if (!cols[col] && !diags1[diag1] && !diags2[diag2]) {
+      state[row][col] = 'Q'
+      cols[col] = diags1[diag1] = diags2[diag2] = true
+      backtrack(row + 1, n, state, res, cols, diags1, diags2)
+      state[row][col] = '#'
+      cols[col] = diags1[diag1] = diags2[diag2] = false
     }
-    if (i > start && choices[i] === choices[i-1]) {
-      continue
-    }
-    state.push(choices[i])
-    backtrack(state, target - choices[i], choices, i + 1, res)
-    state.pop()
   }
 }
 
-/* 求解子集和 II */
-function subsetSumII(nums: number[], target: number): number[][] {
-  const state = []
-  nums.sort((a, b) => a - b)
-  const start = 0
-  const res = []
-  backtrack(state, target, nums, start, res)
+/* 求解 n 皇后 */
+function nQueens(n: number): string[][][] {
+  const state = Array.from({ length: n }, () => Array(n).fill('#'))
+  const cols = Array(n).fill(false)
+  const diag1 = Array(2 * n - 1).fill(false)
+  const diag2 = Array(2 * n - 1).fill(false)
+  const res: string[][][] = []
+
+  backtrack(0,n, state, res, cols, diag1, diag2)
   return res
 }
 
 
-console.log(subsetSumII([3, 4, 5], 9));
+console.log(nQueens(4))
